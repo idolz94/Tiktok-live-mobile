259eddd48fcfafe29bf32b7d6727f9c847f16327
hainm149889 <hainm149889@gmail.com>
2026-05-30 15:13:21 +0700

    config-all: upgrade Expo SDK + react native from 52 -> lastest

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./"],
+          root: ['./'],
           alias: {
-            "@": "./src"
-          }
-        }
-      ]
-    ]
+            '@': './src',
+          },
+        },
+      ],
+
+      'react-native-reanimated/plugin',
+    ],
   };
 };
